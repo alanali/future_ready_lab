3e52483632f1c77fdd7f0af17cee81c24a2baac7
alanali <you@example.comalana.li@oath.com>
2018-06-25 13:54:55 -0700

    Finished number guessing v1

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -2,15 +2,19 @@
 
 function playGame() {
     
+    //Ask for number range
     var low = parseInt(prompt("Enter a low number: "));
     var high = parseInt(prompt("Enter a high number: "));
-
+    
+    //Generate random number in range
     var target = Math.round(Math.random() * (high - low) + low);
-
+    
+    //Ask for guess
     var userGuess  = parseInt(prompt("Enter a guess between " + low + " and " + high));
 
     var totalGuess = 1;
-
+    
+    //Keep asking and giving hints until number guessed
     while (userGuess !== target) {
         if (userGuess < target) {
             userGuess = parseInt(prompt("Your guess is too low: "));
@@ -20,7 +24,8 @@ function playGame() {
             totalGuess++;
         }
     }
-
+    
+    //End of game
     alert("You took " + totalGuess + " guesses to guess the number " + target);
     
 }
