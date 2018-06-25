839cdc867dc5b3b491b8fd97f4881f7dc1ac191b
alanali <you@example.comalana.li@oath.com>
2018-06-25 13:49:22 -0700

    Finished

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -1,1 +1,26 @@
 /*eslint-env browser*/
+
+function playGame() {
+    
+    var low = parseInt(prompt("Enter a low number: "));
+    var high = parseInt(prompt("Enter a high number: "));
+
+    var target = Math.round(Math.random() * (high - low) + low);
+
+    var userGuess  = parseInt(prompt("Enter a guess between " + low + " and " + high));
+
+    var totalGuess = 1;
+
+    while (userGuess !== target) {
+        if (userGuess < target) {
+            userGuess = parseInt(prompt("Your guess is too low: "));
+            totalGuess++;
+        } if (userGuess > target) {
+            userGuess = parseInt(prompt("Your guess is too high: "));
+            totalGuess++;
+        }
+    }
+
+    alert("You took " + totalGuess + " guesses to guess the number " + target);
+    
+}
