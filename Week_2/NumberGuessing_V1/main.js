1639df32785960e0f1bda05faa3f050baf030568
alanali <you@example.comalana.li@oath.com>
2018-06-26 11:46:24 -0700

    Finished renovations

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -2,19 +2,42 @@
 
 function playGame() {
     
-    //Ask for number range
-    var low = parseInt(prompt("Enter a low number: "));
-    var high = parseInt(prompt("Enter a high number: "));
+    var good = false
     
+    //Ask for number range and check if input is number
+    while (good == false) {
+        while (isNaN(low)) {
+            var low = parseInt(prompt("Enter a low number: "));
+            while (low < 0){
+                low = parseInt(prompt("Enter a number greater than 0: "));
+            }
+        } while (isNaN(high)) {
+            var high = parseInt(prompt("Enter a high number: "));
+            while (high > 1000){
+                high = parseInt(prompt("Enter a number less than 1000: "));
+            }
+        }
+        if (low >= high) {
+            alert("The lower bond must be lower than the upper bond.");
+            var temp = low;
+            low = high;
+            high = temp;
+        } else {
+            good = true;
+        }
+    }
+
     //Generate random number in range
     var target = Math.round(Math.random() * (high - low) + low);
     
-    //Ask for guess
-    var userGuess  = parseInt(prompt("Enter a guess between " + low + " and " + high));
-
-    var totalGuess = 1;
+    var userGuess = parseInt(prompt("Enter a guess between " + low + " and " + high));
     
     //Keep asking and giving hints until number guessed
+    while (userGuess < low || userGuess > high) {
+        userGuess = parseInt(prompt("Enter a guess between " + low + " and " + high));
+    }
+    var totalGuess = 1;
+    
     while (userGuess !== target) {
         if (userGuess < target) {
             userGuess = parseInt(prompt("Your guess is too low: "));
