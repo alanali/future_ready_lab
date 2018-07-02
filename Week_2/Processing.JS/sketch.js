34a023124700630a539feb7dc4448b3cfea3b6c2
alanali <you@example.comalana.li@oath.com>
2018-07-02 14:01:05 -0700

    Bouncing Colorful Circles

--- Week_2/Processing.JS/sketch.js
+++ Week_2/Processing.JS/sketch.js
@@ -1,4 +1,4 @@
-//Circle Stuff
+//Circle variables
 var circle = {
     diameter: 80,
     xCoor: 0,
@@ -7,29 +7,29 @@ var circle = {
     xSpeed: 5,
     ySpeed: 5
 }
+
 //Setup
 function setup() {
   createCanvas(640, 480);
-    background("#fae");
+    background("#fae"); //pink
 }
 //Draw function
 function draw() {
     fill(circle.color);
     ellipse(circle.xCoor, circle.yCoor, circle.diameter);
-    
-    console.log("The x coordinate is " + circle.xCoor);
-    //Detect edges and bounce
+
+    //Detect edges and bounce, change direction
     if (circle.xCoor >= 640){
-        circle.color = [random(225), random(225), random(225)];
+        circle.color = [random(225), random(225), random(225)]; //random color
         circle.xSpeed = -circle.xSpeed;
         circle.xCoor += circle.xSpeed;
-        circle.diameter = random(20,100);
+        circle.diameter = random(20,100); //random diameter
     }else if (circle.yCoor >= 480){
         circle.color = [random(225), random(225), random(225)];
         circle.ySpeed = -circle.ySpeed;
         circle.yCoor += circle.ySpeed;
         circle.diameter = random(20,100);
-    }else if (circle.yCoor < 0){
+    }else if (circle.yCoor < 0){ //detect edge
         circle.color = [random(225), random(225), random(225)];
         circle.ySpeed = -circle.ySpeed;
         circle.yCoor += circle.ySpeed;
